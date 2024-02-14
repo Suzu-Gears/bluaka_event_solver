5236108747c1ae83e398b4aff160daa9dbfdc4a5
Suzu <47468734+Suzu-Gears@users.noreply.github.com>
2024-02-14 13:20:27 +0900

    行番号と列番号によって生成するテキストフィールドを自動で切り替える機能を実装。addRowとremoveRowの対象とする行の位置を変更。

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class DynamicLayoutScreen extends StatefulWidget {
 
 class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
   // テキストフィールドのリストを管理するための二次配列
-  List<List<TextField>> textFieldGrid = [];
+  List<List<Widget>> textFieldGrid = [];
   // テキストフィールドのコントローラーの二次配列
   List<List<TextEditingController>> controllersGrid = [];
 
@@ -57,17 +57,20 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
   void initializeGrid() {
     // 初期状態で4行3列のテキストフィールドを追加
     const List<List<String>> gridText = [
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""],
-      ["", "", "", ""]
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""]
     ];
     createGrid(gridText);
   }
 
   void addRow() {
     setState(() {
-      List<TextField> newRow = [];
+      List<Widget> newRow = [];
       List<TextEditingController> newControllersRow = [];
       int numberOfRows = controllersGrid.length;
       int numberOfColmuns = controllersGrid.first.length;
@@ -75,10 +78,13 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
         TextEditingController newController = TextEditingController();
         newControllersRow.add(newController);
         newRow.add(createTextField(
-            controller: newController, row: numberOfRows, col: col));
+            controller: newController,
+            row: numberOfRows,
+            col: col,
+            numberOfRows: numberOfRows));
       }
-      textFieldGrid.add(newRow);
-      controllersGrid.add(newControllersRow);
+      textFieldGrid.insert(numberOfRows - 2, newRow);
+      controllersGrid.insert(numberOfRows - 2, newControllersRow);
     });
   }
 
@@ -89,18 +95,23 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
       for (int row = 0; row < numberOfRows; row++) {
         TextEditingController newController = TextEditingController();
         textFieldGrid[row].add(createTextField(
-            controller: newController, row: row, col: numberOfColmuns));
+            controller: newController,
+            row: row,
+            col: numberOfColmuns,
+            numberOfRows: numberOfRows));
         controllersGrid[row].add(newController);
       }
     });
   }
 
   void removeRow() {
-    // 残り1行は削除しない
-    if (textFieldGrid.length > 1) {
+    // 残り3行以下では削除しない
+    if (textFieldGrid.length > 3) {
       setState(() {
-        textFieldGrid.removeLast();
-        controllersGrid.removeLast();
+        // 下から3行目の要素を削除する
+        int removeIndex = textFieldGrid.length - 3;
+        textFieldGrid.removeAt(removeIndex);
+        controllersGrid.removeAt(removeIndex);
       });
     }
   }
@@ -125,15 +136,18 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
     int numberOfColmuns = gridText.first.length;
     for (int row = 0; row < numberOfRows; row++) {
       List<String> rowText = gridText[row];
-      List<TextField> newRow = [];
+      List<Widget> newRow = [];
       List<TextEditingController> newControllersRow = [];
       for (int col = 0; col < numberOfColmuns; col++) {
         String cellText = rowText[col];
         TextEditingController newController =
             TextEditingController(text: cellText);
         newControllersRow.add(newController);
-        newRow.add(
-            createTextField(controller: newController, row: row, col: col));
+        newRow.add(createTextField(
+            controller: newController,
+            row: row,
+            col: col,
+            numberOfRows: numberOfRows));
       }
       textFieldGrid.add(newRow);
       controllersGrid.add(newControllersRow);
@@ -152,15 +166,72 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
     });
   }
 
-  TextField createTextField(
+  Widget createTextField(
       {required TextEditingController controller,
       required int row,
-      required int col}) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: '入力してください',
+      required int col,
+      required int numberOfRows}) {
+    int flex = 1;
+    bool enabled = true;
+    InputDecoration decoration = InputDecoration(
+      border: OutlineInputBorder(),
+      disabledBorder: InputBorder.none,
+      isDense: true,
+      contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 6.0),
+      hintText: '0',
+    );
+    TextInputType keyboadType = TextInputType.number;
+
+    if (col == 0) {
+      flex = 2;
+      keyboadType = TextInputType.text;
+      decoration = decoration.copyWith(
+        hintText: 'Stage Name',
+      );
+    }
+    if (row == 0) {
+      if (col == 0 || col == 1) {
+        decoration = decoration.copyWith(
+          hintText: col == 0 ? 'Stage' : 'AP',
+          hintStyle: TextStyle(color: Colors.black),
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 6.0),
+        );
+        enabled = false;
+      } else {
+        decoration = decoration.copyWith(
+          hintText: 'Item',
+        );
+      }
+    }
+    if (row == numberOfRows - 2) {
+      if (col == 0 || col == 1) {
+        decoration = decoration.copyWith(
+          hintText: col == 0 ? '' : '初期数',
+          hintStyle: TextStyle(color: Colors.black),
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 6.0),
+        );
+        enabled = false;
+      }
+    }
+    if (row == numberOfRows - 1) {
+      if (col == 0 || col == 1) {
+        decoration = decoration.copyWith(
+          hintText: col == 0 ? '' : '目標数',
+          hintStyle: TextStyle(color: Colors.black),
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 6.0),
+        );
+        enabled = false;
+      }
+    }
+
+    return Expanded(
+      flex: flex,
+      child: TextField(
+        controller: controller,
+        enabled: enabled,
+        decoration: decoration,
+        keyboardType: keyboadType,
+        inputFormatters: [],
       ),
     );
   }
@@ -248,11 +319,9 @@ class _DynamicLayoutScreenState extends State<DynamicLayoutScreen> {
             Flexible(
               child: SingleChildScrollView(
                 child: Column(
-                  children: textFieldGrid.map((List<TextField> row) {
+                  children: textFieldGrid.map((List<Widget> row) {
                     return Row(
-                      children: row
-                          .map((textField) => Expanded(child: textField))
-                          .toList(),
+                      children: row,
                     );
                   }).toList(),
                 ),

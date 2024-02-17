a7a316ccb680a82f146ee327953e1fa15b341c39
Suzu <47468734+Suzu-Gears@users.noreply.github.com>
2024-02-17 09:07:59 +0900

    最大幅の指定。

--- lib/views/dynamic_layout_grid.dart
+++ lib/views/dynamic_layout_grid.dart
@@ -71,75 +71,79 @@ class _DynamicLayoutGridState extends State<DynamicLayoutGrid> {
     return Container(
         alignment: Alignment.center,
         padding: const EdgeInsets.all(4),
-        child: Column(
-          children: [
-            TopControlButtons(
-              onAddRow: _addRow,
-              onRemoveRow:
-                  _gridController.textFieldGrid.length > 4 ? _removeRow : null,
-              onAddColumn: _addColumn,
-              onRemoveColumn: _gridController.textFieldGrid.first.length > 3
-                  ? _removeColumn
-                  : null,
-            ),
-            Row(
-              children: _gridController.textFieldGrid.first,
-            ),
-            Flexible(
-              child: Scrollbar(
-                thumbVisibility: true,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: _gridController.textFieldGrid
-                        .sublist(1, _gridController.textFieldGrid.length - 2)
-                        .map((List<Widget> row) {
-                      return Row(
-                        children: row,
-                      );
-                    }).toList(),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 1000),
+          child: Column(
+            children: [
+              TopControlButtons(
+                onAddRow: _addRow,
+                onRemoveRow: _gridController.textFieldGrid.length > 4
+                    ? _removeRow
+                    : null,
+                onAddColumn: _addColumn,
+                onRemoveColumn: _gridController.textFieldGrid.first.length > 3
+                    ? _removeColumn
+                    : null,
+              ),
+              Row(
+                children: _gridController.textFieldGrid.first,
+              ),
+              Flexible(
+                child: Scrollbar(
+                  thumbVisibility: true,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: _gridController.textFieldGrid
+                          .sublist(1, _gridController.textFieldGrid.length - 2)
+                          .map((List<Widget> row) {
+                        return Row(
+                          children: row,
+                        );
+                      }).toList(),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Row(
-              children: _gridController
-                  .textFieldGrid[_gridController.textFieldGrid.length - 2],
-            ),
-            Row(
-              children: _gridController.textFieldGrid.last,
-            ),
-            BottomControlButtons(
-              onSave: () async {
-                saveText(converControllerGridToString(
-                    _gridController.controllersGrid));
-              },
-              onLoad: () async {
-                final String? loadedText = await loadText();
-                loadedText != null
-                    ? _createGrid(parseStringToListOfLists(loadedText))
-                    : showCustomDialog(context, '読み取り結果', 'データが保存されていません。');
-              },
-              onDisplayArray: () {
-                showCustomDialog(
-                    context,
-                    '配列の内容',
-                    converControllerGridToString(
-                        _gridController.controllersGrid));
-              },
-              onReset: _resetGrid,
-              onCalc: () {
-                SolverLogic solverLogic = SolverLogic(
-                    stageList: gridTextToStageList(fillWithZero(
-                        parseStringToListOfLists(converControllerGridToString(
-                            _gridController.controllersGrid)))),
-                    itemList: gridTextToItemList((fillWithZero(
-                        parseStringToListOfLists(converControllerGridToString(
-                            _gridController.controllersGrid))))),
-                    context: context);
-                solverLogic.calculateSolver();
-              },
-            ),
-          ],
+              Row(
+                children: _gridController
+                    .textFieldGrid[_gridController.textFieldGrid.length - 2],
+              ),
+              Row(
+                children: _gridController.textFieldGrid.last,
+              ),
+              BottomControlButtons(
+                onSave: () async {
+                  saveText(converControllerGridToString(
+                      _gridController.controllersGrid));
+                },
+                onLoad: () async {
+                  final String? loadedText = await loadText();
+                  loadedText != null
+                      ? _createGrid(parseStringToListOfLists(loadedText))
+                      : showCustomDialog(context, '読み取り結果', 'データが保存されていません。');
+                },
+                onDisplayArray: () {
+                  showCustomDialog(
+                      context,
+                      '配列の内容',
+                      converControllerGridToString(
+                          _gridController.controllersGrid));
+                },
+                onReset: _resetGrid,
+                onCalc: () {
+                  SolverLogic solverLogic = SolverLogic(
+                      stageList: gridTextToStageList(fillWithZero(
+                          parseStringToListOfLists(converControllerGridToString(
+                              _gridController.controllersGrid)))),
+                      itemList: gridTextToItemList((fillWithZero(
+                          parseStringToListOfLists(converControllerGridToString(
+                              _gridController.controllersGrid))))),
+                      context: context);
+                  solverLogic.calculateSolver();
+                },
+              ),
+            ],
+          ),
         ));
   }
 }
